b5d2759812e2797631332870c4893de9e4f83e3f
John Udang <john.udang@livetilesglobal.com>
2025-09-01 20:31:47 +0800

    Initial Commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,49 @@
+import SearchForm from "@/components/SearchForm";
+import StartupCard, {StartupTypeCard} from "@/components/StartupCard";
+import { SanityLive, sanityFetch } from "@/sanity/lib/live";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import {auth} from "@/auth";
+
+
+export default async function Home({searchParams}: {searchParams: Promise<{query?: string}>}) {
+
+  const query = (await searchParams).query;
+  const params = {search: query || null};
+
+  const session = await auth();
+
+  console.log(session?.id);
+
+  const {data: posts} = await sanityFetch({query: STARTUPS_QUERY, params});
+
+  return (
+    <div>
+      <section className="pink_container pattern">
+        <h1 className="heading">Pitch Your Startup, <br/> Connect With Entrepreneurs</h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
+        </p>
+
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : `All Startups`}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )
+          }
+        </ul>
+      </section>
+      <SanityLive />
+    </div>
+  );
+}
---
+++ instrumentation-client.ts
@@ -0,0 +1,36 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://82ff0e2e7c94b68dc484878238509d8c@o4509801927606272.ingest.us.sentry.io/4509801929179136",
+
+  // Add optional integrations for additional features
+  integrations: [
+    Sentry.replayIntegration(),
+    Sentry.feedbackIntegration({
+      // Additional SDK configuration goes in here, for example:
+      colorScheme: "system",
+    })
+  ],
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Define how likely Replay events are sampled.
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
+
+  // Define how likely Replay events are sampled when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
